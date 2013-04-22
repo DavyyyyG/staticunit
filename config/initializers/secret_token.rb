2c51c01ccde7123a4ba3dda81634843d5ac2228e
David A. Gomez-Dimas <davyyyyg@gmail.com>
2013-04-22 16:22:45 -0700

    created rales app and added the updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticunitCom::Application.config.secret_token = '54e14600a9b484b27d98c44b16dedfba368a9b168246d3b1ecab0f2aedfa2d961ee8ec758a476565b6f68ec39991c5b2ee3bac686ca26e5ab38391f981908183'
